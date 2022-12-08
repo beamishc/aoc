6b51cf69594cd5d945d4386f6e12032a6723857c
Catriona Beamish <catriona.beamish@gmail.com>
2022-12-08 23:59:42 +0000

    day 8 part 2

--- day_8/aoc8.py
+++ day_8/aoc8.py
@@ -21,3 +21,39 @@ def day_8_1(input):
     print(visible)
 
 day_8_1('day_8/input.txt')
+
+def day_8_2(input):
+
+    with open(input) as f:
+        forest = f.readlines()
+
+    forest_list = []
+    for trees in forest:
+        forest_list.append([int(tree) for tree in trees.strip()])
+
+    forest_grid = np.array(forest_list)
+    scenic = []
+
+    for i, tree in np.ndenumerate(forest_grid):
+        left = forest_grid[i[0],:i[1]][::-1]
+        right = forest_grid[i[0],(i[1]+1):]
+        up = forest_grid[:i[0],i[1]][::-1]
+        down = forest_grid[(i[0]+1):,i[1]]
+        end = 1
+        view = {
+            'left': {'len':len(left), 'cutoff':[n for n,i in enumerate(left) if i >= tree]},
+            'right': {'len':len(right), 'cutoff':[n for n,i in enumerate(right) if i >= tree]},
+            'up': {'len':len(up), 'cutoff':[n for n,i in enumerate(up) if i >= tree]},
+            'down': {'len':len(down), 'cutoff':[n for n,i in enumerate(down) if i >= tree]},
+            }
+        for direction in view.keys():
+            if view[direction]['len'] == 0:
+                end = 0
+            if not view[direction]['cutoff'] and view[direction]['len'] > 0:
+                end = end * view[direction]['len']
+            if view[direction]['cutoff']:
+                end = end * (view[direction]['cutoff'][0] + 1)
+        scenic.append(end)
+    print(max(scenic))
+
+day_8_2('day_8/input.txt')
