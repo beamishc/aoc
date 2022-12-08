13bfb35db57eb261a3725474a642beafd3a51421
Catriona Beamish <catriona.beamish@gmail.com>
2022-12-08 20:06:18 +0000

    day 8 part 1

--- day_8/aoc8.py
+++ day_8/aoc8.py
@@ -1,9 +1,23 @@
+import numpy as np
 def day_8_1(input):
 
     with open(input) as f:
         forest = f.readlines()
 
-    for tree_rows in forest:
-        pass
+    forest_list = []
+    for trees in forest:
+        forest_list.append([int(tree) for tree in trees.strip()])
 
-day_8_1('day_6/input.txt')
+    forest_grid = np.array(forest_list)
+    visible = 0
+
+    for i, tree in np.ndenumerate(forest_grid):
+        left = all(x < tree for x in forest_grid[i[0],:i[1]])
+        right = all(x < tree for x in forest_grid[i[0],(i[1]+1):])
+        up = all(x < tree for x in forest_grid[:i[0],i[1]])
+        down = all(x < tree for x in forest_grid[(i[0]+1):,i[1]])
+        if (left + right + up + down) >= 1:
+            visible += 1
+    print(visible)
+
+day_8_1('day_8/input.txt')
