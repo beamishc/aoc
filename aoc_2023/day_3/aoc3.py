91b2379e7e8c73ef7e2a944e1ff973f5a3a7ddb3
Catriona Beamish <catriona.beamish@gmail.com>
2023-12-03 23:50:21 +0000

    adding comments to day 3 for future me's sanity

--- aoc_2023/day_3/aoc3.py
+++ aoc_2023/day_3/aoc3.py
@@ -2,19 +2,25 @@ from icecream import ic
 from functools import reduce
 import numpy as np
 
+# Creating a quick function to check if any direction was a symbol
 def is_symbol(direction):
     if len(direction) > 0:
         if direction[0] != '.' and not direction[0].isdigit():
             return True
     return False
 
+# Creating a quick function for part 2 to check if any direction was a digit
+# (in a way that was more useful immediately to me than .isdigit() because of the inclusion of the if statement)
 def is_dig(direction):
     if len(direction) > 0:
         if direction[0].isdigit():
             return True
     return False
 
+# Got halfway through part 1 and wished I'd done a recursion, but oh well...
 def day_3_1(textfile):
+
+    # getting the info out of the text file and into a np.array
     with open(textfile) as f:
         lines = f.readlines()
 
@@ -23,6 +29,8 @@ def day_3_1(textfile):
         part_list.append([part for part in parts.strip()])
 
     part_grid = np.array(part_list)
+
+    # variables of the edges
     maxs = part_grid.shape
     max_row = maxs[0] - 2
     max_col = maxs[1] - 2
@@ -30,15 +38,20 @@ def day_3_1(textfile):
     nums = []
 
     for i, part in np.ndenumerate(part_grid):
+
+        # why do any work if it isn't a digit? Thinking with my brain here
         if part.isdigit():
 
-            E = part_grid[i[0],:i[1]][::-1]
-            W = part_grid[i[0],(i[1]+1):]
+            # getting a list of every thing in each compass direction from the coord
             N = part_grid[:i[0],i[1]][::-1]
+            E = part_grid[i[0],:i[1]][::-1]
             S = part_grid[(i[0]+1):,i[1]]
+            W = part_grid[i[0],(i[1]+1):]
 
+            # setting default values for the diagonal points
             NE, NW, SW, SE = ['.', '.', '.', '.']
 
+            # checking diagonals were valid at edges
             if i[0] > 0 and i[1] < max_col:
                 NE = [part_grid[i[0]-1][i[1]+1]]
 
@@ -51,21 +64,29 @@ def day_3_1(textfile):
             if i[0] < max_row and i[1] < max_col:
                 SE = [part_grid[i[0]+1][i[1]+1]]
 
+            # creating a list of all the compass lists to feed into the for loop
             directions = [E, NE, SE, W, NW, SW, N, S]
+
+            # setting up number to be manipulated
             relevant_num = part
 
+            # looping through compass lists to check for symbols
+            # if any symbols, then continue
             if sum([is_symbol(direction) for direction in directions]) > 0:
+                # loop through east and add any digits to left, else break
                 for e in E:
                     if e.isdigit():
                         relevant_num = e + relevant_num
                     else:
                         break
+                # loop through west and add any digits to right, else break
                 for w in W:
                     if w.isdigit():
                         relevant_num = relevant_num + w
                     else:
                         break
 
+                # check that number isn't already in master list, then append
                 if len(nums) >0:
                     if int(relevant_num) != nums[-1]:
                         nums.append(int(relevant_num))
@@ -168,7 +189,6 @@ def day_3_2(textfile):
                             gear_nums.append(int(relevant_num))
                     else:
                         gear_nums.append(int(relevant_num))
-
             if len(gear_nums) > 1:
                 final_nums += reduce(lambda x, y: x*y, gear_nums)
 
