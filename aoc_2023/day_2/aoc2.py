4b5abfb840de509819ce1285e2811c18300e7299
Catriona Beamish <catriona.beamish@gmail.com>
2023-12-04 00:02:41 +0000

    adding comments to day 2 for future me's sanity

--- aoc_2023/day_2/aoc2.py
+++ aoc_2023/day_2/aoc2.py
@@ -7,15 +7,27 @@ def day_2_1(textfile):
     relevant_games = []
 
     for line in lines:
+
+        # create a dictionary for each colour
         max_counts = {'red': 0, 'green': 0, 'blue': 0}
+
+        # string manipulation to get game_no and results
         start, content = line.strip().split(':')
-        results = list(content.split('; '))
-        for result in results:
+
+        # for loop through results
+        for result in list(content.split('; ')):
             ball_counts = list(result.split(', '))
+
+            # for loop through number of balls for each result
             for count in ball_counts:
                 amount, type = count.strip().split()
+
+                # check if current result amount is greater than dictionary amount
                 if int(amount) > max_counts[type]:
+                    # replace if so
                     max_counts[type] = int(amount)
+
+        # nested if statements to check for required minimum number of balls and append if met
         if max_counts['red'] <= 12:
             if max_counts['green'] <= 13:
                 if max_counts['blue'] <= 14:
@@ -25,6 +37,7 @@ def day_2_1(textfile):
 
 print(day_2_1('aoc_2023/day_2/input.txt'))
 
+
 def day_2_2(textfile):
     with open(textfile) as f:
         lines = f.readlines()
@@ -32,14 +45,25 @@ def day_2_2(textfile):
     total = 0
 
     for line in lines:
+        # create dictionary for minimum counts of colours
         mins = {'red': 0, 'green': 0, 'blue': 0}
+
+        # string manipulation to get game_no and results
         results = list(line.strip().split(':')[1].split('; '))
+
+        # loop through results
         for result in results:
             ball_counts = list(result.split(', '))
+
+            # for loop through number of balls for each result
             for count in ball_counts:
                 amount, type = count.strip().split()
+
+                # check if current result amount is greater than dictionary amount
                 if int(amount) > mins[type]:
                     mins[type] = int(amount)
+
+        # add products to total
         total += mins['red'] * mins['green'] * mins['blue']
 
     return total
