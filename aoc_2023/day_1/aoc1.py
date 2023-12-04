9972bb5d05bfebde7ab73cbdcdcf5a4f0d074fc8
Catriona Beamish <catriona.beamish@gmail.com>
2023-12-04 00:07:22 +0000

    adding comments to day 1 for future me's sanity

--- aoc_2023/day_1/aoc1.py
+++ aoc_2023/day_1/aoc1.py
@@ -21,33 +21,39 @@ nums = {'one': '1',
         'seven': '7',
         'eight': '8',
         'nine': '9'}
-ic.enable()
+
 def day_1_2(textfile):
     with open(textfile) as f:
         lines = f.readlines()
 
         total = 0
+
         for line in lines:
-            ic(line)
+
+            # searching for the words from the dict
             for num in nums:
+
+                # replacing second letter to allow for overlap e.g. 'oneight'
                 if line.find(num) != -1:
                     i = line.find(num)
                     line = line[:i+1] + nums[num] + line[i + 2:]
+
+                    # searching again from previous find point - gross but effective
+                    # this could/should have been a recursion
                     if line.find(num, i) != -1:
                         i = line.find(num)
                         line = line[:i+1] + nums[num] + line[i + 2:]
+
+                        # searching again from previous find point - gross but effective
+                        # this could/should have been a recursion
                         if line.find(num, i) != -1:
                             i = line.find(num)
                             line = line[:i+1] + nums[num] + line[i + 2:]
-            ic(line)
+
             digits = [x for x in line if x.isdigit()]
-            ic(digits)
-            # if len(digits) > 1:
-            ic(int(digits[0] + digits[-1]))
+
             total += int(digits[0] + digits[-1])
-            # else:
-            #     total += int(digits[0])
-            ic(total)
+
         return total
 
 print(day_1_2('aoc_2023/day_1/input.txt'))
